93e56c9648fd80dce2d788330e72de36d3d1467c
Alex Tran <vietnogi@gmail.com>
2015-10-02 08:45:10 -0700

    build

--- dist/main.js
+++ dist/main.js
@@ -143,6 +143,7 @@
     return {
       restrict: 'E',
       templateUrl: 'views/layout-project-nav.directive.html',
+      controller: 'ProjectNavController as vm',
       scope: {
         workId: '@workId'
       }
@@ -155,6 +156,37 @@
 
 }).call(this);
 
-angular.module("appirio-tech-ng-work-layout").run(["$templateCache", function($templateCache) {$templateCache.put("views/layout-header.directive.html","<a ng-home-link=\"ng-home-link\" href=\"{{ vm.homeHref }}\" class=\"clean logo\">ASP</a><ul class=\"links\"><li><a ui-sref=\"view-work-multiple\">Dashboard</a></li><li class=\"projects\"><button focus-on-click=\"focus-on-click\" class=\"clean\">Projects &dtrif;</button><ul class=\"sublinks elevated\"><li><a ui-sref=\"submit-work\">Create New Project</a></li><li ng-repeat=\"project in vm.projects\"><a ui-sref=\"timeline({ workId: project.id })\">{{ project.name }}</a></li></ul></li><li ng-hide=\"vm.loggedIn\"><a ui-sref=\"login\">Log in</a></li><li ng-show=\"vm.loggedIn\" class=\"profile\"><button focus-on-click=\"focus-on-click\" class=\"clean\"><avatar></avatar></button><ul class=\"sublinks elevated\"><li><a ng-click=\"vm.logout()\">Logout</a></li><li><a ui-sref=\"submit-work\">Settings</a></li></ul></li><li ng-show=\"vm.loggedIn\" class=\"notifications\"><button type=\"button\" focus-on-click=\"focus-on-click\" class=\"clean\"><div ng-show=\"vm.unreadCount &gt; 0\" class=\"notification\">{{ vm.unreadCount }}</div></button><div class=\"popup elevated\"><threads subscriber-id=\"{{ vm.subscriberId }}\"></threads></div></li></ul><h1 class=\"app-name\">{{ vm.appName }}</h1>");
+(function() {
+  'use strict';
+  var ProjectNavController;
+
+  ProjectNavController = function($scope, $state, StepsAPIService) {
+    var activate, activateLink, vm;
+    vm = this;
+    vm.workId = $scope.workId;
+    activateLink = function() {
+      var isSubmission, stateName;
+      stateName = $state.current.name;
+      isSubmission = stateName === 'submissions' || stateName === 'final-designs' || stateName === 'final-designs' || stateName === 'final-fixes';
+      if (isSubmission) {
+        return vm.activeLink = 'submissions';
+      } else {
+        return vm.activeLink = stateName;
+      }
+    };
+    activate = function() {
+      activateLink();
+      return vm;
+    };
+    return activate();
+  };
+
+  ProjectNavController.$inject = ['$scope', '$state', 'StepsAPIService'];
+
+  angular.module('appirio-tech-ng-work-layout').controller('ProjectNavController', ProjectNavController);
+
+}).call(this);
+
+angular.module("appirio-tech-ng-work-layout").run(["$templateCache", function($templateCache) {$templateCache.put("views/layout-header.directive.html","<a ng-home-link=\"ng-home-link\" href=\"{{ vm.homeHref }}\" class=\"clean logo\">ASP</a><ul class=\"links\"><li ng-show=\"vm.loggedIn\"><a ui-sref=\"view-work-multiple\">Dashboard</a></li><li ng-show=\"vm.loggedIn\" class=\"projects\"><button focus-on-click=\"focus-on-click\" class=\"clean\">Projects &dtrif;</button><ul class=\"sublinks elevated\"><li><a ui-sref=\"submit-work\">Create New Project</a></li><li ng-repeat=\"project in vm.projects\"><a ui-sref=\"timeline({ workId: project.id })\">{{ project.name }}</a></li></ul></li><li ng-hide=\"vm.loggedIn\"><a ui-sref=\"login\">Log in</a></li><li ng-show=\"vm.loggedIn\" class=\"profile\"><button focus-on-click=\"focus-on-click\" class=\"clean\"><avatar></avatar></button><ul class=\"sublinks elevated\"><li><a ng-click=\"vm.logout()\">Logout</a></li><li><a ui-sref=\"submit-work\">Settings</a></li></ul></li><li ng-show=\"vm.loggedIn\" class=\"notifications\"><button type=\"button\" focus-on-click=\"focus-on-click\" class=\"clean\"><div ng-show=\"vm.unreadCount &gt; 0\" class=\"notification\">{{ vm.unreadCount }}</div></button><div class=\"popup elevated\"><threads subscriber-id=\"{{ vm.subscriberId }}\"></threads></div></li></ul><h1 class=\"app-name\">{{ vm.appName }}</h1>");
 $templateCache.put("views/layout-footer.directive.html","<footer class=\"layout-footer\"><ul><li><a ui-sref=\"register\">Sign up</a></li><li><a ui-sref=\"#\">Help</a></li><li><a ui-sref=\"#\">About</a></li></ul></footer>");
-$templateCache.put("views/layout-project-nav.directive.html","<ul><li class=\"active\"><a ui-sref=\"timeline({ workId: workId })\">Timeline</a></li><li><a ui-sref=\"submissions({ id: workId })\">Submissions</a></li><li><a ui-sref=\"messaging({ id: workId })\">Messaging</a></li><li><a ui-sref=\"project-requirements({ id: workId })\">Project reqs</a></li><li><a ui-sref=\"project-settings({ id: workId })\">Project settings</a></li></ul>");}]);
+$templateCache.put("views/layout-project-nav.directive.html","<ul><li ng-class=\"{active: vm.activeLink == \'timeline\'}\"><a ui-sref=\"timeline({ workId: vm.workId })\">Timeline</a></li><li ng-class=\"{active: vm.activeLink == \'submissions\'}\"><a ui-sref=\"submissions({ projectId: vm.workId })\">Submissions</a></li><li ng-class=\"{active: vm.activeLink == \'messaging\'}\"><a ui-sref=\"messaging({ id: vm.workId })\">Messaging</a></li><li ng-class=\"{active: vm.activeLink == \'project-requirements\'}\"><a ui-sref=\"project-requirements({ id: vm.workId })\">Project reqs</a></li><li ng-class=\"{active: vm.activeLink == \'project-settings\'}\"><a ui-sref=\"project-settings({ id: workId })\">Project settings</a></li></ul>");}]);
